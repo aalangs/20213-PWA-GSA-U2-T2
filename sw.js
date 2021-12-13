a2b465408cb21dece11554be373122e0c8fa8ad7
Alan Garcia <aalangarcia.1998@gmail.com>
2021-12-12 19:55:48 -0600

    Cambio de path

--- sw.js
+++ sw.js
@@ -1,28 +1,22 @@
-let path = '/20213-PWA-GSA-U2-T2/sw.js';
-
 const CACHE_STATIC = "static_v1"
 const CACHE_INMUTABLE = "inmutable_v1"
 
 self.addEventListener('install', (event) => {
-    let url = self.location.href;
-    if (url.includes('localhost') || url.includes('127.0.0.1')) {
-        path = '/';
-    }
 
     const cacheStatic = caches.open(CACHE_STATIC)
     .then( cache => {
         cache.addAll([
-            path + './',
-            path + './index.html',
-            path + './manifest.json',
-            path + './js/camera.js',
-            path + './js/app.js',
-            path + './images/icons/android-launchericon-48-48.png',
-            path + './images/icons/android-launchericon-72-72.png',
-            path + './images/icons/android-launchericon-96-96.png',
-            path + './images/icons/android-launchericon-144-144.png',
-            path + './images/icons/android-launchericon-192-192.png',
-            path + './images/icons/android-launchericon-512-512.png',
+            '/20213-PWA-GSA-U2-T2/',
+            '/20213-PWA-GSA-U2-T2/index.html',
+            '/20213-PWA-GSA-U2-T2/manifest.json',
+            '/20213-PWA-GSA-U2-T2/js/camera.js',
+            '/20213-PWA-GSA-U2-T2/js/app.js',
+            '/20213-PWA-GSA-U2-T2/images/icons/android-launchericon-48-48.png',
+            '/20213-PWA-GSA-U2-T2/images/icons/android-launchericon-72-72.png',
+            '/20213-PWA-GSA-U2-T2/images/icons/android-launchericon-96-96.png',
+            '/20213-PWA-GSA-U2-T2/images/icons/android-launchericon-144-144.png',
+            '/20213-PWA-GSA-U2-T2/images/icons/android-launchericon-192-192.png',
+            '/20213-PWA-GSA-U2-T2/images/icons/android-launchericon-512-512.png',
         ])
     })
 
